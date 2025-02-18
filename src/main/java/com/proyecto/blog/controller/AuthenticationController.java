53bc7460c510f77199fc5ca926e0659ee5bd2f7c
Tu Nombre <tu-email@dominio.com>
2025-02-18 05:13:20 -0300

    Añadí la clase AuthenticationController en el paquete controller

---
+++ src/main/java/com/proyecto/blog/controller/AuthenticationController.java
@@ -0,0 +1,28 @@
+package com.proyecto.blog.controller;
+
+import com.proyecto.blog.dto.AuthLoginRequestDTO;
+import com.proyecto.blog.dto.AuthResponseDTO;
+import com.proyecto.blog.service.UserDetailsServiceImp;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+    @Autowired
+    private UserDetailsServiceImp userDetailsService;
+
+    //Todas estas requests y responses vamos a tratarlas como dto
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponseDTO> login (@RequestBody @Valid AuthLoginRequestDTO userRequest) {
+        return new ResponseEntity<>(this.userDetailsService.loginUser(userRequest), HttpStatus.OK);
+    }
+
+}
