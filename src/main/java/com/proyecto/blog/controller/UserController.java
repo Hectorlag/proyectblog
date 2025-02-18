335deb738715bac434db24a7ff4515090551680e
Tu Nombre <tu-email@dominio.com>
2025-02-18 05:29:47 -0300

    Modifiqué el path en el método post de la clase userController

--- src/main/java/com/proyecto/blog/controller/UserController.java
+++ src/main/java/com/proyecto/blog/controller/UserController.java
@@ -51,7 +51,7 @@ public class UserController {
         )).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @PostMapping("/api/user")
+    @PostMapping()
     public ResponseEntity<UserSecResponseDTO> createUser(@RequestBody UserDTO userDTO,
                                                          @RequestParam boolean isAuthor,
                                                          String authorName) {
