cb3dd2291d25c009b061c274166fce33de674f8c
Tu Nombre <tu-email@dominio.com>
2025-02-18 05:23:40 -0300

    Añadí la Annotation @Component a la clase JwtUtils

--- src/main/java/com/proyecto/blog/utils/JwtUtils.java
+++ src/main/java/com/proyecto/blog/utils/JwtUtils.java
@@ -9,12 +9,14 @@ import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
 
 import java.util.Date;
 import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+@Component
 public class JwtUtils {
 
     @Value("${security.jwt.private.key}")
