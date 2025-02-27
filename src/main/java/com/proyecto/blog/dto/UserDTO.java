deaa938ae3852447795f57c2c3266c4535fab6fa
Tu Nombre <tu-email@dominio.com>
2025-02-27 17:45:42 -0300

    Modifiqué las validaciones con annotations en los atributos de la clase UserDTO

--- src/main/java/com/proyecto/blog/dto/UserDTO.java
+++ src/main/java/com/proyecto/blog/dto/UserDTO.java
@@ -1,6 +1,7 @@
 package com.proyecto.blog.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,18 +13,23 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter @Setter
-public class UserDTO {
 
-    @NotBlank(message = "El nombre de usuario es obligatorio")
-    @Size(min = 3, max = 50, message = "El nombre de usuario debe tener entre 3 y 50 caracteres")
-    private String username;
+    public class UserDTO {
 
-    @NotBlank(message = "La contraseña es obligatoria")
-    @Size(min = 6, message = "La contraseña debe tener al menos 6 caracteres")
-    private String password;
+        @NotBlank(message = "El nombre de usuario es obligatorio")
+        @Size(min = 3, max = 50, message = "El nombre de usuario debe tener entre 3 y 50 caracteres")
+        private String username;
 
-    @Size(min = 3, max = 100, message = "El nombre del autor debe tener entre 3 y 100 caracteres")
-    private String authorName;
-    private boolean isAuthor;  // Indicador de si el usuario es un AUTHOR
-    private List<String> roles;  // Para asignar ADMIN manualmente
+        @NotBlank(message = "La contraseña es obligatoria")
+        @Size(min = 6, message = "La contraseña debe tener al menos 6 caracteres")
+        private String password;
+
+        @Size(min = 3, max = 100, message = "El nombre del autor debe tener entre 3 y 100 caracteres")
+        private String authorName;
+
+        private boolean isAuthor;
+
+        @NotEmpty(message = "El usuario debe tener al menos un rol")
+        private List<String> roles;
+
 }
