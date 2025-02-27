a06c8928814f8c07b06c0246cd87e4c4ffa39437
Tu Nombre <tu-email@dominio.com>
2025-02-27 17:48:45 -0300

    Añadí un método en la clase GlobalExceptionHandler

--- src/main/java/com/proyecto/blog/excepcion/GlobalExceptionHandler.java
+++ src/main/java/com/proyecto/blog/excepcion/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.proyecto.blog.excepcion;
 
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -18,6 +19,21 @@ public class GlobalExceptionHandler {
 
         ex.getBindingResult().getFieldErrors().forEach(error ->
                 errors.put(error.getField(), error.getDefaultMessage())
+        );
+
+        response.put("message", "Error de validación");
+        response.put("errors", errors);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Map<String, Object>> handleConstraintViolationExceptions(ConstraintViolationException ex) {
+        Map<String, Object> response = new HashMap<>();
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getConstraintViolations().forEach(violation ->
+                errors.put(violation.getPropertyPath().toString(), violation.getMessage())
         );
 
         response.put("message", "Error de validación");
